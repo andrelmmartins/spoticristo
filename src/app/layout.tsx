c695ec3dbcb891ea24c61296fced7740ba655e56
André Martins <dev11@beeteller.com>
2024-07-23 11:34:45 -0300

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>EAC music</title>
+      </head>
       <body className={font.className}>{children}</body>
     </html>
   );
